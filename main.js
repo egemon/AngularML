1bcd51f2bd4173596fbc415942d9a33563b88e6f
Ilya Lykianov <lykianovib@gmail.com>
2015-10-31 00:29:59 +0200

    new

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 (function () {
     var app = angular.module('todoapp', []);
-    // console.log = function (string) {
-    //     $('body').append(string);
-    // };
 
     app.controller('ListCtr', function($scope){
         this.formatTime = function () {
-            arguments = Array.prototype.slice.call(arguments)
-            return (new Date(arguments)).getTime()
+            return (new Date(Array.prototype.slice.call(arguments))).getTime()
         }
 
 
@@ -21,7 +17,7 @@
             doneable: true,
             img: {
                 small:'friends.jpg'
-            }
+            },
         },{
             task: 'Think about Helloween',
             timeline: this.formatTime(2015, 9, 30),
@@ -35,26 +31,25 @@
     });
 
     app.controller('PanelController', function(){
-        this.tab = 1;
         this.selectTab = function (tab) {
             this.tab = tab;
-        }
+        };
         this.isSelected = function  (tab) {
             return this.tab == tab;
-        }
-    })
+        };
+    });
 
     app.filter('task',function(){
         return function (str) {
             console.log('args = ', arguments);
             return 'Summary:' + str;
-        }
+        };
     });
     app.filter('desc',function(){
         return function (str) {
             console.log('args = ', arguments);
             return 'You should: ' + str;
-        }
+        };
     });
 
 
